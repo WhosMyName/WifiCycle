42c0b1b198e107026702b1855392dcd4426b0fad
WhosMyName <chikangwu95@gmail.com>
2018-08-09 11:01:02 +0200

    added super shit-loads of template functions

--- app/src/main/java/com/koro/wificycle/MainActivity.kt
+++ app/src/main/java/com/koro/wificycle/MainActivity.kt
@@ -9,21 +9,83 @@ class MainActivity : AppCompatActivity() {
     //val scanTimer: Float = loadFromSave
     //val wifiState: String? = loadFromSettings //loads current Wifi State from ????
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { // handle as init function?
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    /*fun loadFromSave(????????????){
-    *
-    * // *insert some shit on loading from saves here*
-    *
-    * }
-    *
-    *
-    *
-    * */
+    override fun onStart() { //handle as load state
+        super.onStart()
+    }
+
+    override fun onResume(){
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //saveToSave(???????)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() { // save everything here, shouldn't be called to often
+        super.onDestroy()
+        //saveTosave(???????)
+    }
+
+/*
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+    }
+
+*/
+
+/*
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState")
+    }
+*/
+
+/*
+    fun loadFromSave(????????????){
+
+    // *insert some shit on loading from saves here*
+
+    }
+*/
+
+/*
+    fun saveToSave(??????){
+
+     // *insert saving schnannigans here*
+
+     }
+*/
+
 }
+
+
+/*
+fun getWiFiStatus(???){//returns if wifi is (e|n|u|m)
+
+    //returns wifi status
+
+}
+*/
+
+/*
+fun setWiFiStatus(????, state){ //sets status of Wifi to (on/off)
+
+    // set Wifi staus
+
+}
+*/
+
 
 /*
 * Changeable Timers:
